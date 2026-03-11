598699bba3d9502208f66ea1853a0a6a02b18b67
San Ha Seo <sanha_seo@hotmail.com>
2026-03-10 22:07:49 -0500

    Add terms of service page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
 import { PrivacyComponent } from './components/privacy/privacy.component';
+import { TermsComponent } from './components/terms/terms.component';
 
 export const routes: Routes = [
     { path: 'privacy', component: PrivacyComponent },
+    { path: 'terms', component: TermsComponent },
     { path: '', component: GameComponent },
     { path: ':date', component: GameComponent }
 ];
