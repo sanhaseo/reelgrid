ba4b91940ca08b0c8cbc2c6d34b651abd1d89488
San Ha Seo <sanha_seo@hotmail.com>
2026-03-10 19:57:08 -0500

    Add privacy policy page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
 
 export const routes: Routes = [
+    { path: 'privacy', component: PrivacyComponent },
     { path: '', component: GameComponent },
     { path: ':date', component: GameComponent }
 ];
---
+++ src/app/components/privacy/privacy.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-privacy',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './privacy.component.html',
+  styleUrl: './privacy.component.css'
+})
+export class PrivacyComponent {
+  currentDate = new Date();
+}
